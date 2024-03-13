cec0e92da565f6657c4bfa136e3155fb64aa72ba
altalha201 <talha.cse201@gmail.com>
2024-03-13 12:45:39 +0600

    update

--- backend/utils/genarateToken.js
+++ backend/utils/genarateToken.js
@@ -9,7 +9,7 @@ const genarateTokenAndSetCookie = (userId, res) => {
         maxAge: 15 * 24 * 60 * 60 * 1000, // Milisec
         httpOnly: true, // prevent XSS attacks cross-site scripting attacks
         sameSite: "strict", // CSRF attacks cross-site request forgery attacks
-        sequre: process.env.NODE_ENV !== "development",
+        sequre: true,
     });
 };
 
